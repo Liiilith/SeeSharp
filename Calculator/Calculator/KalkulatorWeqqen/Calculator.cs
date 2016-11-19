fbeb9e51847c54a93cc00d715d9cb5661c499a41
weqqen <grzegorz.nowak@otchlan.pl>
2016-11-19 21:11:49 +0100

    Folder Weqqena

---
+++ Calculator/Calculator/KalkulatorWeqqen/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.KalkulatorWeqqen
+{
+    class Calculator : ICalculator
+    {
+        public string getDisplay()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void sendData(char data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
